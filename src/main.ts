44a7ede4fd89975b9ea7da21b4ca80f53c46efef
Joseph Afriyie Attakorh <juxluvjoe@gmail.com>
2024-12-24 13:56:49 +0000

    feat: setup prisma, docker, swagger, config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Unleashed-YMS API')
+    .setDescription('Unleashed YMS API description')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(3000);
 }
 bootstrap();
