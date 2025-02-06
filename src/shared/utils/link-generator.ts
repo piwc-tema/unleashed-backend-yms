8231f2f98c449f64eb73ae040d85f72e2b91129a
Joseph Afriyie Attakorh <juxluvjoe@gmail.com>
2025-02-06 21:29:33 +0000

    fix: Link Generation - closes #4

--- src/shared/utils/link-generator.ts
+++ src/shared/utils/link-generator.ts
@@ -1,7 +1,8 @@
 import { v4 as uuid } from 'uuid';
+import * as process from 'process';
 
 const generateRegistrationLink = (id: string = uuid()): string => {
-  return `http://localhost:4200/form/${id}`;
+  return `${process.env.APP_FRONTEND_URL}/forms/${id}`;
 };
 
 export default generateRegistrationLink;
